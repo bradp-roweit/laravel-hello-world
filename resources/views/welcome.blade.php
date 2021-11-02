c8ad468d1f7b3cfe43b08658d4ea0677a2debac4
Bradley Phillips <bradley.phillips@roweit.co.uk>
2021-11-02 16:04:35 +0000

    Initial commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+
+        <title>Hello world!</title>
+
+    </head>
+    <body class="antialiased">
+        <h1>Hello World!</h1>
+    </body>
+</html>
